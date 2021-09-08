19ee6dbedd753db6520e7ee9cc5ba44118013aab
xiguayang <yangzhangamber1990@gmail.com>
2021-09-07 20:51:15 -0700

    Project 8-2 Streamy. Google Auth for login/out, add Redux form
    Using Google Auth gapi.auth2 to login or out, using Redux to track the SignIn status and also the userId.
    Make use of Redux form for creating and submitting forms, add form validation for input and show error information for input

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
-ReactDOM.render(<App />, document.querySelector('#root'));
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+);
