e23a416b39740c07f5b8b150027cd029117fc71f
xiguayang <yangzhangamber1990@gmail.com>
2021-09-08 21:15:29 -0700

    8-3 Adding API Server and Use RESTFul for Show Stream &Create Stream & Update Stream
    Using json-server as the API server in local 3001. Using Rest conventions for creating and showing, editing streams. (axios) bundle up all action creators for CURD and use object based Reducers. Using history reference for navigation. Create StreamForm to be reused in both createStream and editStream

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,6 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import streams from '../APIs/streams';
+import history from '../history';
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAM, FETCH_STREAMS, DELETE_STREAM, EDIT_STREAM } from './types';
 export const signIn = (userId) => {
 	return {
 		type: SIGN_IN,
@@ -10,3 +12,68 @@ export const signOut = () => {
 		type: SIGN_OUT,
 	};
 };
+
+// export const createStream = (formValues) => {
+// 	return (dispatch) => {};
+// };
+
+export const createStream = (formValues) => async (dispatch, getState) => {
+	const { userId } = getState().auth;
+	const response = await streams.post('/streams', { ...formValues, userId });
+	dispatch({ type: CREATE_STREAM, payload: response.data });
+	//Do some programmatic navigation to
+	//get the user back to the root route
+	history.push('/');
+};
+
+export const fetchStreams = () => async (dispatch) => {
+	const response = await streams.get('/streams');
+	dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+	const response = await streams.get(`/streams/${id}`);
+	dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+	const response = await streams.patch(`/streams/${id}`, formValues);
+	dispatch({ type: EDIT_STREAM, payload: response.data });
+	history.push('/');
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+	await streams.delete(`/streams/${id}`);
+	dispatch({ type: DELETE_STREAM, payload: id });
+};
+
+//Sample Code
+//Array-based approach
+// const streamReducer = (state = [], action) => {
+// 	switch (action.type) {
+// 		case EDIT_STREAM:
+// 			return state.map((stream) => {
+// 				if (stream.id === action.payload) {
+// 					return action.payload;
+// 				} else {
+// 					return stream;
+// 				}
+// 			});
+// 		default:
+// 			return state;
+// 	}
+// };
+
+//Object-based approach
+// const streamReducer = (state = {}, action) => {
+// 	switch (action.type) {
+// 		case EDIT_STREAM:
+// 			// const newState = { ...state };
+// 			// newState[action.payload.id] = action.payload;
+// 			// return newState;
+// 			//using es25 sytax
+// 			return { ...state, [action.payload.id]: action.payload };
+// 		default:
+// 			return state;
+// 	}
+// };
--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,6 +1,76 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-	return <div>StreamList</div>;
+class StreamList extends React.Component {
+	componentDidMount() {
+		this.props.fetchStreams();
+	}
+
+	//a helper method to check the userId inside stream equals to auth.userId or not
+	renderAdmin(stream) {
+		if (stream.userId === this.props.currentUserId) {
+			return (
+				<div className="right floated content">
+					{/* <button className="ui button primary">EDIT</button> */}
+					<Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+						Edit
+					</Link>
+					<Link to={`/streams/delete/${stream.id}`} className="ui button negative">
+						Delete
+					</Link>
+				</div>
+			);
+		}
+	}
+
+	renderCreate() {
+		if (this.props.isSignedIn) {
+			return (
+				<div style={{ textAlign: 'right' }}>
+					<Link to="/streams/new" className="ui button primary">
+						CREATE STREAM
+					</Link>
+				</div>
+			);
+		}
+	}
+
+	renderList() {
+		return this.props.streams.map((stream) => {
+			return (
+				<div className="item" key={stream.id}>
+					{this.renderAdmin(stream)}
+					<i className="large middle aligned icon camera" />
+					<div className="content">
+						{stream.title}
+						<div className="description">{stream.description}</div>
+					</div>
+				</div>
+			);
+		});
+	}
+	render() {
+		//console.log(this.props.streams);
+		return (
+			<div>
+				<h2>Stream</h2>
+				<div className="ui celled list">{this.renderList()}</div>
+				{this.renderCreate()}
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state) => {
+	//Object.values(obj):built-in JS function that takes an object as argument
+	//all different values inside the object will be pulled out  and then inserted into an array
+	return {
+		streams: Object.values(state.streams),
+		currentUserId: state.auth.userId,
+		isSignedIn: state.auth.isSignedIn,
+	};
 };
-export default StreamList;
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
